be89842969625ac8194e7dc8acae2a1c533dab7a
Akira <coruscationmail@gmail.com>
2026-01-31 18:59:08 +0800

    clean deps.edn

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((cider-clojure-cli-aliases . ":test:example:customized-graal:build"))))
+((nil . ((cider-clojure-cli-aliases . ":test:example:build"))))
